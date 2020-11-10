893dc741b98103034cf60bd7540864cd4bffe6f9
esther parnes <eparnes123@gmail.com>
2020-11-10 11:58:35 +0200

    Add project files.

---
+++ NuritTargilLesson5/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NuritTargilLesson5
+{
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+
+      private void WindowLoad(object sender, RoutedEventArgs e)
+      {
+         //open second window
+
+         SecondWindow secondWindow = new SecondWindow();
+         secondWindow.Show();
+      }
+      private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+      {
+         if  (((ListBox)sender).SelectedItem == "") //check if nothing was selected
+         {
+            TextBoxAdd.Visibility = Visibility.Visible;
+            ButtonAdd.Visibility = Visibility.Visible;
+            ButtonAdd.IsEnabled = false;
+         }
+         else
+         //chose something so hide the controls
+         {
+            TextBoxAdd.Visibility = Visibility.Hidden;
+            ButtonAdd.Visibility = Visibility.Hidden;
+            MessageBox.Show("you have chosen " + ListBoxCities.SelectedItem);
+
+         }
+      }
+
+      private void TextChanged_Event(object sender, TextChangedEventArgs e)
+      {
+         
+         ButtonAdd.IsEnabled = true;//make button visible then there is text changed
+      }
+
+      private void ButtonClick_Event(object sender, RoutedEventArgs e)
+      {
+         //button click adds the name to the list
+          ListBoxCities.Items.Add(TextBoxAdd.Text);
+         // ListBoxCities = word;
+      }
+
+      private void closeButton_Click(object sender, RoutedEventArgs e)
+      {
+         this.Close();
+      }
+
+   }
+}
---
+++ NuritTargilLesson5/SecondWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NuritTargilLesson5
+{
+   /// <summary>
+   /// Interaction logic for SecondWindow.xaml
+   /// </summary>
+   public partial class SecondWindow : Window
+   {
+      public SecondWindow()
+      {
+         InitializeComponent();
+      }
+
+
+
+
+
+      private void RadioButtonYellow_Checked(object sender, RoutedEventArgs e)
+      {
+         MessageBox.Show("Yellow light");
+      }
+
+      private void RadioButtonRed_Checked(object sender, RoutedEventArgs e)
+      {
+         MessageBox.Show("REd light");
+      }
+
+      private void RadioButtonGreen_Checked(object sender, RoutedEventArgs e)
+      {
+         MessageBox.Show("Green light");
+      }
+
+      private void ButtonThirdWindow_Click(object sender, RoutedEventArgs e)
+      {
+
+         ThirdWindow thirdWindow = new ThirdWindow();
+         thirdWindow.Show();
+         this.Close();
+      }
+
+
+      private void ButtonFirstWindow_Click(object sender, RoutedEventArgs e)
+      {
+         MainWindow firstWindow = new MainWindow();
+         firstWindow.Show();
+         this.Close();
+
+      }
+   }
+}
